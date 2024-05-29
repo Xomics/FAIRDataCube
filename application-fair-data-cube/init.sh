5abdfeed59b56486c32e17629b0473eb8b87754d
Yuliia Orlova <yuliia.orlova@surf.nl>
2024-05-29 13:37:59 +0200

    fixed Vantage6 server

--- application-fair-data-cube/init.sh
+++ application-fair-data-cube/init.sh
@@ -4,7 +4,6 @@
 nginx_conf="/etc/nginx/conf.d/ssl_main.conf"
 
 # Let Nginx serve the docker container running on port 8080 instead of a static web page
-# sed -i 's|root /var/www/html;|location \/ {\n    \tproxy_pass http:\/\/127.0.0.1:7200;\n    \tproxy_set_header Host $host;\n    \tproxy_set_header X-Real-IP $remote_addr;\n    } \n    location ~ \/fairdatapoint(\/.*)?$ {\n  \trewrite \/fairdatapoint(\/.*) $1 break;\n    \trewrite \/fairdatapoint \/ break;\n    \tproxy_pass http:\/\/127.0.0.1:8080;\n    \tproxy_set_header Host $host;\n    \tproxy_set_header X-Real-IP $remote_addr;\n   \tproxy_pass_request_headers on;\n  } |' "$nginx_conf"
 sed -i 's|root /var/www/html;|location \/ {\n    \tindex index_fdcube.html;\n } |' "$nginx_conf"
 #cat app_ports.conf >> "$nginx_conf"
 sed -i 's|index index.html index.htm;||' "$nginx_conf"
--- vantage6-server/init.sh
+++ vantage6-server/init.sh
@@ -4,7 +4,7 @@
 nginx_conf="/etc/nginx/conf.d/ssl_main.conf"
 
 # Let Nginx serve the docker container running on port 8000 instead of a static web page
-sed -i 's|root /var/www/html;|location \/ {\n    \tproxy_pass http:\/\/localhost:8000;\n    \tproxy_set_header Host $host;\n    \tproxy_set_header X-Real-IP $remote_addr;\n    }|' "$nginx_conf"
+sed -i 's|root /var/www/html;|location \/ {\n    \tproxy_pass http:\/\/localhost:8000;\n    \tproxy_set_header Host $host;\n    \tproxy_set_header X-Real-IP $remote_addr;\n    }\n location \/socket.io {\n    \tinclude proxy_params;\n    \tproxy_http_version 1.1;\n    \tproxy_buffering off;\n    \tproxy_set_header Upgrade $http_upgrade;\n    \tproxy_set_header Connection "Upgrade";\n    \tproxy_pass http:\/\/127.0.0.1:5000\/socket.io;\n    }|' "$nginx_conf"
 sed -i 's|index index.html index.htm;||' "$nginx_conf"
 
 systemctl restart nginx.service
